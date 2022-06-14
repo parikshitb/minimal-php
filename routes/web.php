1f882da977d796e6b520adc1cd4db1cb18ffa9c9
parikshit <parixit23@gmail.com>
2022-06-14 23:25:27 +0530

    already defined middleware by the name "auth" is applied to our only route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+// Route::get('/login', function () {
+//     return view('login');
+// })->name('login');
