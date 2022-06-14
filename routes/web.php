163fa4fadd85f237444cafa4985ae3b50196c1b8
parikshit <parixit23@gmail.com>
2022-06-14 23:34:01 +0530

    documentation for auth middleware

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,19 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| 
+| In other wordings,
+| middlewares and middleware groups are defined in Kernel.php
+| Out of which all the entries in array $middleware and $middlewareGroups['web'] are called.
+| Below that, there are some middlewares provided by Laravel in array $routeMiddlewares
+| We need to execute a middleware by the key 'auth' which is added for this route explicitely.
 |
+| Note:
+| When we include 'auth' middleware, if no guard is mentioned, the default guard is also considered, which is 'web'.
+| Refer to config\Auth.php for this.
+| The code for this can be found in Illuminate\Auth\AuthManager.php
 */
 
 Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
-
-// Route::get('/login', function () {
-//     return view('login');
-// })->name('login');
